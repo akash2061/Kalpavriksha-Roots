f67c0bcd964ac6ff97b5a296741c8e8a9697cc65
Akash Soni <113748706+akash2061@users.noreply.github.com>
2025-10-08 00:26:11 +0530

    Refactor calculator to use full naming conventions & removed comments

--- Assignment_1/calculator.c
+++ Assignment_1/calculator.c
@@ -2,24 +2,24 @@
 #include <ctype.h>
 #include <string.h>
 
-int numDigits(const char *str, int *p){
-    int num = 0;
-    while (isdigit(str[*p])){
-        num = num * 10 + (str[*p] - '0');
-        (*p)++;
+int numDigits(const char *str, int *position){
+    int digits = 0;
+    while (isdigit(str[*position])){
+        digits = digits * 10 + (str[*position] - '0');
+        (*position)++;
     }
-    (*p)--;
-    return num;
+    (*position)--;
+    return digits;
 }
 
 void evaluate(const char *str){
-    int p = 1;
-    int num[128], c = 0;
-    char op[128], o = 0;
+    int position = 1;
+    int digits[128], digit_count = 0;
+    char operators[128], operator_count = 0;
     int start_pos = 0;
     if (str[0] == '-'){
-        num[c++] = -numDigits(str, &p);
-        start_pos = p + 1;
+        digits[digit_count++] = -numDigits(str, &position);
+        start_pos = position + 1;
     }
     if (!isdigit(str[strlen(str) - 2])){
         printf("Foo ERROR: Invalid Expression.\n");
@@ -30,8 +30,7 @@ void evaluate(const char *str){
             continue;
         }
         if (isdigit(str[i])){
-            num[c++] = numDigits(str, &i);
-            // printf("d-%d: %d\n", c - 1, num[c - 1]); //! DEBUG: Digits parsed
+            digits[digit_count++] = numDigits(str, &i);
         }
         else if (str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/'){
             if (i == 0 && str[i] == '-'){
@@ -41,63 +40,60 @@ void evaluate(const char *str){
                 printf("Test ERROR: Invalid Expression. %d\n", i);
                 return;
             }
-            op[o++] = str[i];
-            // printf("o-%d: %c\n", o - 1, op[o - 1]); //! DEBUG: Operators parsed
+            operators[operator_count++] = str[i];
         }
         else{
             printf("Global ERROR: Invalid Expression. %d\n", i);
             return;
         }
-        // printf("%c : num[%d]: %d\n", str[i], c - 1, num[c - 1]); //! Test DEBUG 
     }
-    for (int i = 0; i < o; i++){
-        if (op[i] == '*' || op[i] == '/'){
-            if (op[i] == '*'){
-                num[i] = num[i] * num[i + 1];
+    for (int i = 0; i < operator_count; i++){
+        if (operators[i] == '*' || operators[i] == '/'){
+            if (operators[i] == '*'){
+                digits[i] = digits[i] * digits[i + 1];
             }
             else{
-                if (num[i + 1] == 0){
+                if (digits[i + 1] == 0){
                     printf("ERROR: Division by zero.\n");
                     return;
                 }
-                num[i] = num[i] / num[i + 1];
+                digits[i] = digits[i] / digits[i + 1];
             }
-            for (int j = i + 1; j < c - 1; j++){
-                num[j] = num[j + 1];
+            for (int j = i + 1; j < digit_count - 1; j++){
+                digits[j] = digits[j + 1];
             }
-            for (int j = i; j < o - 1; j++){
-                op[j] = op[j + 1];
+            for (int j = i; j < operator_count - 1; j++){
+                operators[j] = operators[j + 1];
             }
-            c--;
-            o--;
+            digit_count--;
+            operator_count--;
             i--;
         }
     }
-    for (int i = 0; i < o; i++){
-        if (op[i] == '+'){
-            num[i] = num[i] + num[i + 1];
+    for (int i = 0; i < operator_count; i++){
+        if (operators[i] == '+'){
+            digits[i] = digits[i] + digits[i + 1];
         }
-        else if (op[i] == '-'){
-            num[i] = num[i] - num[i + 1];
+        else if (operators[i] == '-'){
+            digits[i] = digits[i] - digits[i + 1];
         }
-        for (int j = i + 1; j < c - 1; j++){
-            num[j] = num[j + 1];
+        for (int j = i + 1; j < digit_count - 1; j++){
+            digits[j] = digits[j + 1];
         }
-        for (int j = i; j < o - 1; j++){
-            op[j] = op[j + 1];
+        for (int j = i; j < operator_count - 1; j++){
+            operators[j] = operators[j + 1];
         }
-        c--;
-        o--;
+        digit_count--;
+        operator_count--;
         i--;
     }
-    printf("Final Result: %d\n", num[0]);
+    printf("Final Result: %d\n", digits[0]);
 }
 
 int main(){
     printf("Enter the Expression:");
     char str[256];
     fgets(str, sizeof(str), stdin);
-    // printf("Input Expression size: %d\n", strlen(str));
     evaluate(str);
     return 0;
 }
